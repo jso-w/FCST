5f93601e983f912750f153dd6520ecd910d3464f
jsow <null>
2025-03-07 16:23:35 +0100

    edit

--- ocr_logger.py
+++ ocr_logger.py
@@ -2,5 +2,8 @@ import pytesseract
 import cv2
 import video_processor
 
-def OcrLogger():
-    def __init__():
+def ocr_frame(frame_roi):
+    frame_text = pytesseract.image_to_string(frame_roi, config='--psm 6')
+    return frame_text
+
+print(cv2.getBuildInformation())
--- video_processor.py
+++ video_processor.py
@@ -1,14 +1,19 @@
+#Processes the video file & stores data
+
 import cv2
+import pytesseract
+from datetime import timedelta
 
 class VideoProcessor:
+
     def __init__(self, video_path):
         self.video_path = video_path
         self.vid = cv2.VideoCapture(self.video_path)
-        if not self.vid.isOpened:
-            raise ValueError("Video could not be loaded. :(")
+        if not self.vid.isOpened():
+            raise ValueError("Video could not be loaded.")
         self.frames_total = self.vid.get(cv2.CAP_PROP_FRAME_COUNT)
         self.fps = self.vid.get(cv2.CAP_PROP_FPS)
-        self.first_frame = self.get_first_frame
+        self.first_frame = self.get_first_frame()
 
     def get_video(self):
         return self.vid
@@ -17,8 +22,8 @@ class VideoProcessor:
         return self.fps
     
     def release_video(self):
-        if self.vid.isOpened:
-            self.vid.release
+        if self.vid.isOpened():
+            self.vid.release()
 
     def get_first_frame(self):
         ret, frame = self.vid.cv2.read()
@@ -26,3 +31,19 @@ class VideoProcessor:
             return False
         return frame
     
+    def run_scan(self, frame_skip, roi):
+        frame_count = 0
+        while(self.vid.isOpened()):
+            check, frame = self.vid.read()
+            if not check:
+                break
+            frame_roi = frame[roi]
+            cv2.cvtColor(frame_roi)
+            cv2.resize(frame_roi, None, fx = 2, fy = 2, interpolation=(cv2.INTER_LINEAR))
+            cv2.threshold(frame_roi, 150, 255, cv2.THRESH_BINARY)
+
+            frame_text = pytesseract.image_to_string(frame_roi, config="--psm 6")
+
+            if frame_text.strip():
+                timecode = str(timedelta(seconds = frame_count / self.fps))
+                frame_output.append({"Time": timecode, })
